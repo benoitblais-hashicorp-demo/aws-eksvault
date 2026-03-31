f1e5c4cadf91f1d2cc29e84327c440c546c5adf9
GitHub Action <notifications@github.com>
2026-03-31 20:16:11 +0000

    Automated Terraform Formatting

--- modules/k8s-demo-app/outputs.tf
+++ modules/k8s-demo-app/outputs.tf
@@ -1,9 +1,9 @@
 output "vault_secret_path" {
-	description = "Vault KVv2 secret path backing the webpage content."
-	value       = "${var.vault_kv_mount_path}/${var.vault_secret_path_prefix}/vso/webapp"
+  description = "Vault KVv2 secret path backing the webpage content."
+  value       = "${var.vault_kv_mount_path}/${var.vault_secret_path_prefix}/vso/webapp"
 }
 
 output "website_url" {
-	description = "Demo webpage URL exposed through the Kubernetes ingress load balancer."
-	value       = try("http://${kubernetes_ingress_v1.static_app.status[0].load_balancer[0].ingress[0].hostname}", "")
+  description = "Demo webpage URL exposed through the Kubernetes ingress load balancer."
+  value       = try("http://${kubernetes_ingress_v1.static_app.status[0].load_balancer[0].ingress[0].hostname}", "")
 }
