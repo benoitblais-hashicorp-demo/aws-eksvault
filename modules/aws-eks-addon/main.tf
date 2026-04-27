93603c2db75b7ad4711481a96d05711665817a92
GitHub Action <notifications@github.com>
2026-04-27 19:07:19 +0000

    Automated Terraform Formatting

--- modules/aws-eks-addon/main.tf
+++ modules/aws-eks-addon/main.tf
@@ -8,10 +8,10 @@ module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
   version = "~> 1.23"
 
-  cluster_name          = var.cluster_name
-  cluster_endpoint      = var.cluster_endpoint
-  cluster_version       = var.cluster_version
-  oidc_provider_arn     = var.oidc_provider_arn
+  cluster_name      = var.cluster_name
+  cluster_endpoint  = var.cluster_endpoint
+  cluster_version   = var.cluster_version
+  oidc_provider_arn = var.oidc_provider_arn
 
   # Keep stack-level dependency inputs consumed to preserve ordering semantics.
   create_delay_dependencies = compact([
@@ -71,10 +71,10 @@ module "eks_blueprints_addons_helm" {
   source  = "aws-ia/eks-blueprints-addons/aws"
   version = "~> 1.23"
 
-  cluster_name          = var.cluster_name
-  cluster_endpoint      = var.cluster_endpoint
-  cluster_version       = var.cluster_version
-  oidc_provider_arn     = var.oidc_provider_arn
+  cluster_name      = var.cluster_name
+  cluster_endpoint  = var.cluster_endpoint
+  cluster_version   = var.cluster_version
+  oidc_provider_arn = var.oidc_provider_arn
 
   depends_on = [module.eks_blueprints_addons]
 
